96bc914d4f826175b602ff0dfff07b1bc427b2c6
KatelevskiyViktor <empirikk@ya.ru>
2023-05-17 20:35:54 +0300

    add widgets, work with multilangueges

--- app/widgets/language/lang_tpl.php
+++ app/widgets/language/lang_tpl.php
@@ -1,12 +1,15 @@
-<div class="dropdown d-inline-block">-->
+<div class="dropdown d-inline-block">
     <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
-        <img src="<?= PATH ?>/assets/img/ru.png" alt="">
+        <img src="<?= PATH ?>/assets/img/lang/<?= \mf\App::$app->getProperty('language')['code'] ?>.png" alt="">
     </a>
     <ul class="dropdown-menu" id="languages">
+        <?php foreach ($this->languages as $k => $v): ?>
+            <?php if ( \mf\App::$app->getProperty('language')['code'] === $k) continue; ?>
+        <?php endforeach; ?>
         <li>
-            <button class="dropdown-item" data-langcode="en">
-                <img src="<?= PATH ?>/assets/img/en.png" alt="">
-                English
+            <button class="dropdown-item" data-langcode="<?= $k ?>">
+                <img src="<?= PATH ?>/assets/img/lang/<?= $k ?>.png" alt="">
+                <?= $v['title'] ?>
             </button>
         </li>
     </ul>
